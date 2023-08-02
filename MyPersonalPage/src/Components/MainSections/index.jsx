d5a5822fb2978cf518889ead1bdfa0b7ab39fc50
Bekzod <bekmuradovbekzod955@gmail.com>
2023-08-02 16:06:08 +0500

    finished main section

--- MyPersonalPage/src/Components/MainSections/index.jsx
+++ MyPersonalPage/src/Components/MainSections/index.jsx
@@ -13,16 +13,17 @@ const MainSections = () => {
                             <h1> Hi, I'm Bekzod Bekmuratov</h1>
                             <span>17 y.o Full-stack developer, from Uzbekistan</span>
                         </div>
-
-                        <div className="projects_card">
-                            <h1>Projects</h1>
-                            <span>I'm learning web programming and posting projects I'm currently working on here</span>
-                            <button>Projects</button>
-                        </div>
-                        <div className="rezume_card">
-                            <h1>About me</h1>
-                            <span>You can find out all the information about me, see my resume and so on</span>
-                            <button>About me</button>
+                        <div className="cards grid grid-cols-2 gap-3">
+                            <div className="projects_card p-6 bg-slate-700">
+                                <h1>Projects</h1>
+                                <span>I'm learning web programming and posting projects I'm currently working on here</span>
+                                <button>Projects</button>
+                            </div>
+                            <div className="rezume_card p-6 bg-slate-700">
+                                <h1>About me</h1>
+                                <span>You can find out all the information about me, see my resume and so on</span>
+                                <button>About me</button>
+                            </div>
                         </div>
                     </div>
                 </div>
