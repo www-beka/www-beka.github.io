5bfaead3c038a3e2814bf75acb5e900847adbda3
Bekzod <bekmuradovbekzod955@gmail.com>
2024-02-12 21:09:24 +0500

    all over again

--- MyPersonalPage/src/App.jsx
+++ MyPersonalPage/src/App.jsx
@@ -1,27 +1,10 @@
-import HomePage from "./Components/Pages/Home";
-import Layout from "./Components/Layout/Layout";
-import NoPage from "./Components/Layout/NoPage";
-import AboutPage from "./Components/Pages/About";
-import Portfolio from "./Components/Pages/Portfolio";
-import Contact from "./Components/Pages/Contact";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
     <>
       <div className="app">
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Layout />}>
-              <Route index element={<HomePage />} />
-              <Route path="About" element={<AboutPage />} />
-              <Route path="Portfolio" element={<Portfolio />} />
-              <Route path="Contact" element={<Contact />} />
-              <Route path="*" element={<NoPage />} />
-            </Route>
-          </Routes>
-        </BrowserRouter>
+        <h1>hello word</h1>
       </div>
     </>
   );
