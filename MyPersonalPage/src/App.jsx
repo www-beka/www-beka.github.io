39d569f2ec8ba0f7954b32a0f99fff5f967a5534
Bekzod <bekmuradovbekzod955@gmail.com>
2024-02-18 21:14:59 +0500

    small changes for Parallax

--- MyPersonalPage/src/App.jsx
+++ MyPersonalPage/src/App.jsx
@@ -1,21 +1,29 @@
-import './app.scss'
-import Cursor from './Components/cursor/Cursor'
-import Navbar from './Components/navbar/Navbar';
-import Hero from './Components/hero/Hero';
-
+import "./app.scss";
+import Cursor from "./Components/cursor/Cursor";
+import Navbar from "./Components/navbar/Navbar";
+import Hero from "./Components/hero/Hero";
+import Services from "./Components/services/Services";
+import Parallax from "./Components/parallax/Parallax";
 
 function App() {
   return (
-      <div className="app">
-        <Cursor />
-        <section id='Homepage'>
-          <Navbar />
-          <Hero />
-        </section>
-        
-        
-      </div>
+    <div className="app">
+      <Cursor />
+      <section id="Homepage">
+        <Navbar />
+        <Hero />
+      </section>
+      <section id="Services">
+        <Parallax type="services" />
+      </section>
+      <section>
+        <Services />
+      </section>
+      <section id="Portfolio">
+        <Parallax type="portfolio" />
+      </section>
+    </div>
   );
 }
 
-export default App; 
+export default App;
