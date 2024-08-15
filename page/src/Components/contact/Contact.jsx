e2488855d61cdb3e29b0cee74270363113ff544d
Bekzod <bekmuradovbekzod955@gmail.com>
2024-08-15 18:50:57 +0500

    small changes

--- page/src/Components/contact/Contact.jsx
+++ page/src/Components/contact/Contact.jsx
@@ -57,15 +57,15 @@ const Contact = () => {
         <motion.h1 variants={variants}>Let’s work together</motion.h1>
         <motion.div className="item" variants={variants}>
           <h2>Mail</h2>
-          <span>hello@react.dev</span>
+          <span>bekmuradovbekzod955@gmail.com</span>
         </motion.div>
         <motion.div className="item" variants={variants}>
           <h2>Address</h2>
-          <span>Hello street New York</span>
+          <span>Hello street Samarkand</span>
         </motion.div>
         <motion.div className="item" variants={variants}>
           <h2>Phone</h2>
-          <span>+1 234 5678</span>
+          <span>+998 91 519 15 30</span>
         </motion.div>
       </motion.div>
       <div className="formContainer">
